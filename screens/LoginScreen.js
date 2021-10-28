52b5739f7a9809a5ff110dda5ec22bcc848da01f
caarlosdamian <dapecarlos@gmail.com>
2021-10-28 09:23:54 -0500

    Feat/Add Login sc reen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,6 +4,7 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
 
@@ -11,11 +12,33 @@ const LoginScreen = () => {
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Email"
-        value={}
-        onChangeText={text =>}
-        style={styles.input}
+        <TextInput
+          placeholder="Email"
+          // value={}
+          // onChangeText={text =>}
+          style={styles.input}
         ></TextInput>
+        <TextInput
+          placeholder="Password"
+          // value={}
+          // onChangeText={text =>}
+          style={styles.input}
+          secureTextEntry
+        ></TextInput>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            // onPress={()=>}
+            style={styles.button}
+          >
+            <Text style={styles.button}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            // onPress={()=>}
+            style={[styles.button, styles.buttonOutline]}
+          >
+            <Text style={styles.buttonOutlineText}>Register</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </KeyboardAvoidingView>
   );
@@ -23,4 +46,10 @@ const LoginScreen = () => {
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+});
